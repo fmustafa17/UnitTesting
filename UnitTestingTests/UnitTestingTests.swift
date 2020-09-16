bcc26a2fab791fb632808b6b065cda8b327a1e68
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-09-15 22:13:41 -0700

    adding in text data and displaying

--- UnitTestingTests/UnitTestingTests.swift
+++ UnitTestingTests/UnitTestingTests.swift
@@ -23,7 +23,13 @@ class UnitTestingTests: XCTestCase {
     
     func testAllWordsLoaded() {
         let playData = PlayData()
-        XCTAssertEqual(playData.allWords.count, 0, "allWords must be 0")
+        XCTAssertEqual(playData.allWords.count, 18440, "allWords must be 18440")
     }
-
+    
+    func testWordCountsAreCorrect() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.wordCounts["home"], 174, "Home does not appear 174 times")
+        XCTAssertEqual(playData.wordCounts["fun"], 4, "Fun does not appear 4 times")
+        XCTAssertEqual(playData.wordCounts["mortal"], 41, "Mortal does not appear 41 times")
+    }
 }
