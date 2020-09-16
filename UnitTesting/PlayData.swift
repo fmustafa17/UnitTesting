a82180578831ede06b38d7f11217d9777f6e3015
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-09-15 21:17:41 -0700

    adding in a tableview

---
+++ UnitTesting/PlayData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayData.swift
+//  UnitTesting
+//
+//  Created by Farhana Mustafa on 9/15/20.
+//  Copyright © 2020 Farhana Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class PlayData {
+    var allWords = [String]()
+}
--- UnitTestingTests/UnitTestingTests.swift
+++ UnitTestingTests/UnitTestingTests.swift
@@ -11,24 +11,19 @@ import XCTest
 
 class UnitTestingTests: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testAllWordsLoaded() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.allWords.count, 0, "allWords must be 0")
     }
 
 }
